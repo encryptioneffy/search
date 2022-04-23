4c4e89086c1e015e435d02e1e8ae6f63bcfcd5e0
Effy Pelayo Tran <effy_pelayo_tran@brown.edu>
2022-04-23 12:49:49 -0400

    design check REPL

---
+++ dcREPL.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__": 
+    while True:
+        user_input = input(">>search")
+        if user_input == ":quit":
+            break
+        print(user_input.upper())
+        
