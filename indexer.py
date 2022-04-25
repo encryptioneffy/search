d81d5524379b4663d127709bbd299a3ba8979415
Effy Pelayo Tran <effy_pelayo_tran@brown.edu>
2022-04-25 19:03:10 -0400

    token stop stem

--- indexer.py
+++ indexer.py
@@ -1,12 +1,13 @@
+from array import array
 import sys
 import file_io
 import xml.etree.ElementTree as et
 import nltk
 import re
 import math
-nltk.download('stopwords')
 from nltk.corpus import stopwords
 STOP_WORDS = set(stopwords.words('english'))
+from nltk.stem import PorterStemmer
 
 class Indexer:
     def __init__(self, xml_file, title_file, doc_file, word_file):
@@ -20,10 +21,51 @@ class Indexer:
         root = et.parse(sys.argv[1]).getroot()
         self.all_pages = root.findall("page")
         self.words_to_id_to_count = {}
-        
-    
-        
 
+    def tokenize_stop_stem(page_text : str) -> list:
+        n_regex = '''\[\[[^\[]+?\]\]|[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
+        l_regex = '''[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
+        nltk_stemmer = PorterStemmer()
+        tokenized_words = []
+        list_of_link_titles = [] #set of all page titles this page_text links to
+
+        # TOKENIZE
+        page_tokens = re.findall(n_regex, page_text)
+
+        for word in page_tokens:
+            # identifying links in text, storing them for when we do PageRank 
+            if re.match('''\[\[[^\[]+?\]\]''', word) != None:
+                if "|" in word:
+                    link_title, link_text = word.split("|")
+                    link_text_tokens = re.findall(n_regex, link_text) 
+
+                    # we can play around with how we wanna store links. 
+                    # Do we want to put it in dict immediately? Or just have
+                    # the helper function output the inner set that will be 
+                    # the value of the linking dict?
+                    # currenty I am just storing it as a set of link titles
+                    link_title = link_title.replace("[[", "")
+                else: 
+                    link_text_tokens = re.findall(l_regex, word)
+                    link_title = word.strip("[[]]")
+                
+                # STOP
+                link_words = filter(lambda x: x not in STOP_WORDS, \
+                    map(str.lower, link_text_tokens))
+                # STEM
+                link_words = map(lambda x: nltk_stemmer.stem(x), link_words)
+                tokenized_words += link_words
+
+                # saves link_title to a data structure, making sure
+                # there are no duplicate links stored for given page_text
+                if link_title not in list_of_link_titles:
+                    list_of_link_titles.append(link_title)
+            else:
+                word = word.lower()
+                if word not in STOP_WORDS:
+                    tokenized_words.append(nltk_stemmer.stem(word))
+        
+        return(tokenized_words, list_of_link_titles)
 
     def make_title_dict(self):
         for page in self.all_pages:
@@ -33,29 +75,34 @@ class Indexer:
         file_io.write_title_file(self.title_file, self.title_dict)
 
     def make_word_dict(self):
-    
-        n_regex = '''\[\[[^\[]+?\]\]|[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
         for page in self.all_pages:
             text: str = page.find("text").text
             id: int = page.find("id").text
-            all_words = re.findall(n_regex, text)
+            # all_words = re.findall(n_regex, text)
+            # all_words is a list of tokenize/stemmed/stopped words for a given page
+            all_words_in_page = self.tokenize_stop_stem(text)[0]
+
         
         #NEED TO STEM AND REMOVE STOP WORDS
             words_to_id_tf = {}
             max_count = 0
-            for word in all_words: 
+
+            for word in all_words_in_page: 
 
                 if word in self.words_to_id_to_count:
-                    #might cause problem w diff ids idk tho
+                    # might cause problem w diff ids idk tho
                     self.words_to_id_to_count[word][id] += 1
                     if self.words_to_id_to_count[word][id] > max_count:
                         max_count = self.words_to_id_to_count[word][id]
                 else:
                     self.words_to_id_to_count[word][id] = 1
-            for word in all_words:
+                    
+            for word in all_words_in_page:
                 words_to_id_tf[word][id] = (self.words_to_id_to_count[word][id]) / max_count
+
         n = len(self.all_pages)
         words_to_idf = {}
+
         for word in self.words_to_id_to_count:
             n_i = len(self.words_to_id_to_count[word])
             words_to_idf[word] = math.log(n/n_i)
---
+++ scratchpaper.py
@@ -0,0 +1,75 @@
+from array import array
+import sys
+import file_io
+import xml.etree.ElementTree as et
+import re
+import math
+
+from nltk.corpus import stopwords
+STOP_WORDS = set(stopwords.words('english'))
+
+from nltk.stem import PorterStemmer
+# nltk_test = PorterStemmer()
+# nltk_test.stem("Stemming")
+
+'''This function successfully tokenizes a really long string into a list 
+of lower case words-strings, with Link Titles removed, multi-word Link Text  
+included in the corpus, and all words stopped & stemmed. '''
+
+# We should play around with how tokenize, stop, stem are organized. Will they 
+# be in separate helper functions? Or altogether in 1 function? Which version
+# will minimize the amount of times we have to loop through a list?
+def tokenize_stop_stem(page_text : str) -> list:
+        n_regex = '''\[\[[^\[]+?\]\]|[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
+        l_regex = '''[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
+        nltk_stemmer = PorterStemmer()
+        tokenized_words = []
+        list_of_link_titles = [] #list of all the page titles this page_text links to
+
+        # TOKENIZE
+        page_tokens = re.findall(n_regex, page_text)
+
+        for word in page_tokens:
+            # identifying links in text, storing them for when we do PageRank later
+            if re.match('''\[\[[^\[]+?\]\]''', word) != None:
+                if "|" in word:
+                    link_title, link_text = word.split("|")
+                    link_text_tokens = re.findall(n_regex, link_text) 
+
+                    # we can play around with how we wanna store links. 
+                    # Do we want to put it in dict immediately? Or just have
+                    # the helper function output the inner set that will be the value
+                    # of the linking dict?
+                    # currenty I am just storing it as a set of link titles
+                    link_title = link_title.replace("[[", "")
+                else: 
+                    link_text_tokens = re.findall(l_regex, word)
+                    link_title = word.strip("[[]]")
+                
+                # STOP
+                link_words = filter(lambda x: x not in STOP_WORDS, \
+                    map(str.lower, link_text_tokens))
+                # STEM
+                link_words = map(lambda x: nltk_stemmer.stem(x), link_words)
+                tokenized_words += link_words
+
+                # saves link_title to a data structure, making sure
+                # there are no duplicate links stored for given page_text
+                if link_title not in list_of_link_titles:
+                    list_of_link_titles.append(link_title)
+            else:
+                word = word.lower()
+                if word not in STOP_WORDS:
+                    tokenized_words.append(nltk_stemmer.stem(word))
+        
+        return(tokenized_words, list_of_link_titles)
+
+# (1) add link_text to the corpus, but make sure to treat
+# like any other text: parse, tokenize, stop, and stem it. 
+# ESPECIALLY is it is multi-word link_text like "how are you"!
+# From EdStem:
+# '[[how are you]]' is an example of a link without a pipe, 
+# for which you would count 'how are you' as a title and 
+# process each word. This means you should treat "how are you" 
+# like any other text: tokenize, remove stop words, stem, etc.
+# (2) add link_title to wherever you are storing page links
