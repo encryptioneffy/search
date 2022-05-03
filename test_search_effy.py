2ff7146d55bd5df2422a0b937d095a91bcbc604d
jessicaliu526 <jessica_j_liu@brown.edu>
2022-05-03 16:35:17 -0400

    fixing testing

--- test_search_effy.py
+++ test_search_effy.py
@@ -4,12 +4,11 @@
 # 2. all the different link tokenizing scenarios
 # 3. tokenizing basic text (no links) """
 # # ids to link ids data structure
-# import index
-# from index import *
-# import pytest
-# from pytest import approx
+from index import *
+import pytest
+from pytest import approx
 
-# def test_parsing_xml():
-#     indexer_ex = Indexer("wikis/test_tf_idf.xml", "title", "doc", "word")
-#     assert len(indexer_ex.all_pages) == 3
-#     # assert that the title & id & text are extracted properly?
+def test_parsing_xml():
+    index_ex = Index("wikis/test_tf_idf.xml", "title", "doc", "word")
+    assert len(index_ex.all_pages) == 3
+    # assert that the title & id & text are extracted properly?
--- test_search_jess.py
+++ test_search_jess.py
@@ -1,24 +1,26 @@
 from index import *
 import pytest
 
-index1 = Index("wikis/test1.xml", "title", "doc", "word")
+def test_tf_idf_pagerank():
+    index1 = Index("wikis/test1.xml", "title", "doc", "word")
 
-index1.make_word_dict()
+    index1.make_word_dict()
 
+#testing tf values
+    assert index1.words_to_id_tf["woof"][1] == 1.0
+    assert index1.words_to_id_tf["dog"][2] == 1.0
+    assert index1.words_to_id_tf["run"][3] == 0.5
+#testing idf values
+    assert index1.words_to_idf["woof"] == 0.0
+    assert index1.words_to_idf["chees"] == 0.22314355131420976
+    assert index1.words_to_idf["run"] == 0.9162907318741551
+#testing max count
+    assert index1.id_to_max_count[1] == 2
+    assert index1.id_to_max_count[2] == 2
+    assert index1.id_to_max_count[3] == 4
+    assert index1.id_to_max_count[4] == 4
+    assert index1.id_to_max_count[5] == 4
 
-assert index1.words_to_id_tf["woof"][1] == 1.0
-assert index1.words_to_id_tf["dog"][2] == 1.0
-assert index1.words_to_id_tf["run"][3] == 0.5
-
-assert index1.words_to_idf["woof"] == 0.0
-assert index1.words_to_idf["chees"] == 0.22314355131420976
-assert index1.words_to_idf["run"] == 0.9162907318741551
-
-assert index1.id_to_max_count[1] == 2
-assert index1.id_to_max_count[2] == 2
-assert index1.id_to_max_count[3] == 4
-assert index1.id_to_max_count[4] == 4
-assert index1.id_to_max_count[5] == 4
 
 
 
